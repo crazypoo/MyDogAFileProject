5a828ded79a9cba03e9c38d1fdebd00a4ac9ed90
crazypoo <273277355@qq.com>
2015-05-06 13:58:04 +0800

    no message

---
+++ MyDogA/MyDogA.h
@@ -0,0 +1,15 @@
+//
+//  MyDogA.h
+//  MyDogA
+//
+//  Created by crazypoo on 15/5/6.
+//  Copyright (c) 2015年 P. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyDogA : NSObject
+
+-(void)helloOne;
++(void)helloTwo;
+@end
---
+++ MyDogA/MyDogA.m
@@ -0,0 +1,22 @@
+//
+//  MyDogA.m
+//  MyDogA
+//
+//  Created by crazypoo on 15/5/6.
+//  Copyright (c) 2015年 P. All rights reserved.
+//
+
+#import "MyDogA.h"
+
+@implementation MyDogA
+
+-(void)helloOne
+{
+    NSLog(@"111");
+}
+
++(void)helloTwo
+{
+    NSLog(@"2");
+}
+@end
